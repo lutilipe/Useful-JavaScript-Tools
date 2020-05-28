5c8db068d856fa6cb6b1f679d3d0ff75f3928329
¨lutilipe¨ <lutilipe02@hotmail.com>
2020-05-28 09:16:08 -0300

    Updates

---
+++ precache-manifest.f43bc5b1f714de0a09bafef66b129159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532de77bc2fafe25576ad0f3c7f0c74c",
+    "url": "/lutilipe/Useful-JavaScript-Tools/index.html"
+  },
+  {
+    "revision": "6b48bde9f3d3c64ce012",
+    "url": "/lutilipe/Useful-JavaScript-Tools/static/css/main.fc8b9a21.chunk.css"
+  },
+  {
+    "revision": "e74b7797f1d67e4043d5",
+    "url": "/lutilipe/Useful-JavaScript-Tools/static/js/2.6db76f4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lutilipe/Useful-JavaScript-Tools/static/js/2.6db76f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b48bde9f3d3c64ce012",
+    "url": "/lutilipe/Useful-JavaScript-Tools/static/js/main.4b0f7c8f.chunk.js"
+  },
+  {
+    "revision": "cf6a88aa5a1d54791331",
+    "url": "/lutilipe/Useful-JavaScript-Tools/static/js/runtime-main.9bd77502.js"
+  }
+]);
